3767505f150364b1afb3ec32c7ffe9f65e5a0d07
Henrik Beck <henrikbeck95@gmail.com>
2023-10-04 18:08:58 -0300

    Closes #27

--- src/main/java/com/mhef/library/spreadsheet/utils/validation/ValidationData.java
+++ src/main/java/com/mhef/library/spreadsheet/utils/validation/ValidationData.java
@@ -4,17 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author Henrik Beck
+ * @version 1.0.0
+ */
 public class ValidationData {
-	public static boolean isRowContentDataEmpty(String[] values) {
-		for (String value : values) {
-			if (!value.trim().isEmpty()) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
+	/**
+	 * @param row The spreadsheet matrix data from a single row.
+	 * @return The verification about if all the values from a single row is empty (null) or not.
+	 */
 	public static boolean isRowContentDataEmptyOrNullable(List<String> row) {
 		boolean isEmptyRow = true;
 
@@ -32,6 +30,10 @@ public class ValidationData {
 		return false;
 	}
 
+	/**
+	 * @param args The arguments informed from the command line interface(CLI).
+	 * @return The verification about if there is or there is not arguments.
+	 */
     public static boolean isThereArguments(String[] args) {
         if (args.length == 0) {
             System.out.println("Please provide the filename as an argument.");
@@ -41,6 +43,11 @@ public class ValidationData {
         }
     }
 
+	/**
+	 * @param dateDouble The date as double format. In example, the 42598.0 value.
+	 * @param syntaxFormat The date format syntax. In example, the "M/d/yy hh:ss" value
+	 * @return The converted value based on the date format syntax. In example, "8/16/16 12:00" value
+	 */
 	public static String convertFromDateToString(Date dateDouble, String syntaxFormat) {
 		return new SimpleDateFormat(syntaxFormat).format(dateDouble);
 	}
--- src/main/java/com/mhef/library/spreadsheet/utils/validation/ValidationFile.java
+++ src/main/java/com/mhef/library/spreadsheet/utils/validation/ValidationFile.java
@@ -3,11 +3,23 @@ package com.mhef.library.spreadsheet.utils.validation;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * @author Henrik Beck
+ * @version 1.0.0
+ */
 public class ValidationFile {
+	/**
+	 * @param filePath The path to the informed file.
+	 * @return The verification is the file exists or not
+	 */
 	public static boolean fileExists(String filePath) {
 		return new java.io.File(filePath).exists();
 	}
 
+	/**
+	 * @param filePath The path to the informed file.
+	 * @return The file extension value. In example, ".csv", ".xls" and "xlsx" values.
+	 */
 	public static String getFileExtension(String filePath) {
 		Path path = Paths.get(filePath);
 		String fileName = path.getFileName().toString();
